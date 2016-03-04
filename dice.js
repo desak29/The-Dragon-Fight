2eeb83fa1e30c5c2246fc8c369f68d3e63a3894d
desak29 <desak29@icloud.com>
2016-03-04 10:27:33 -0500

    starting to improve
    -need to fix slight js  issue
    -improve format layout

---
+++ dice.js
@@ -0,0 +1,20 @@
+
+<script type="text/javascript">
+function throwDice(){
+	var randomDie = Math.round(Math.random)*5+1;
+// creates a number between 1 and 6
+	var imageName = "IMG/d"+randomDie+".gif";
+
+document.images['myDie1'].src = imageName;
+}
+var randomDie2 = Math.round(Math.random)*5+1;
+// creates a number between 1 and 6
+	var imageName2 = "IMG/d"+randomDie2+".gif";
+document.images['myDie2'].src = imageName2;	
+
+}
+var random total = randomDie +randomDie2;
+if(random Total<= 9)
+	document.getElementById=""
+
+</script>
